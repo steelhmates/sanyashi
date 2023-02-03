d92b99e395901d654c247e7835122b468da21c8f
techknowlogick <techknowlogick@gitea.io>
2023-02-02 23:30:20 -0500

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,158 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Gitea Documentation',
+  tagline: 'Git with a cup of tea',
+  url: 'https://docs.gitea.com',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.png',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'/*, 'fr-fr', 'zh-cn', 'zh-tw'*/], // temporarily disable other locales
+  },
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      //'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/', // Serve the docs at the site's root
+          editUrl: ({locale, versionDocsDirPath, docPath}) => {
+            return `https://github.com/go-gitea/gitea/tree/main/docs/content/doc/${docPath}`
+          },
+          versions: {
+            current: {
+              label: '1.19-dev',
+              //path: '1.19-dev', // path is next (so users can always find "nightly" docs)
+            },
+//             '1.17': {
+//               banner: 'none',
+//             },
+          },
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+  themes: [
+    [
+      "@easyops-cn/docusaurus-search-local",
+      {
+        hashed: false,
+        language: ["en"],
+        highlightSearchTermsOnTargetPage: true,
+        explicitSearchResultPath: true,
+        indexBlog: false,
+        docsRouteBasePath: "/"
+      }
+    ]
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: true,
+        respectPrefersColorScheme: true,
+      },
+      announcementBar: {
+        id: 'announcementBar-1', // Increment on change
+        content: `The updated documentation site is a work in progress. If you find any issues, please <a target="_blank" rel="noopener noreferrer" href="https://github.com/go-gitea/gitea/issues">let us know</a>. Translated documentation is also a work in progress.`,
+      },
+      navbar: {
+        title: 'Gitea',
+        logo: {
+          alt: 'Gitea Logo',
+          src: 'img/gitea.svg',
+        },
+        items: [
+          {
+            to: '/',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            href: 'https://github.com/go-gitea/gitea',
+            label: 'Code',
+            position: 'left',
+          },
+          {
+            type: 'search',
+            position: 'right',
+          },
+          {
+            type: 'docsVersionDropdown',
+            position: 'right',
+            dropdownActiveClassDisabled: true,
+          },
+          {
+            to: 'help/seek-help',
+            position: 'right',
+            label: 'Support',
+            activeBaseRegex: 'help/seek-help',
+          }
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/gitea',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/gitea',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/giteaio',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Code',
+                href: 'https://github.com/go-gitea/gitea',
+              },
+            ],
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ loop_docs.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -xe
+
+sed -i 's/<empty/<&#8288;empty/' docs/advanced/config-cheat-sheet.en-us.md
+sed -i 's/<empty/<&#8288;empty/' docs/developers/guidelines-backend.en-us.md
+sed -i 's/</<&#8288;/' docs/developers/guidelines-backend.en-us.md
+sed -i 's/^url:.*//' docs/intro.md
+sed -i 's/^title:.*/displayed_sidebar: mySidebar/' docs/intro.md
+sed -i 's/^slug:.*/slug: \//' docs/intro.md
+sed -i 's/.\/guidelines-frontend.md/.\/guidelines-frontend/' docs/developers/hacking-on-gitea.en-us.md
+
+sed -i 's/"version":.*/"version":"1.19-dev"/' static/latest-swagger.json
+
+for file in `find ./docs/ -name "*.md"`; do
+    # note only works on linux, forget about it when attempting to run on macos
+    # hide hugo toc
+    sed -i 's/{{< toc >}}//' $file
+    sed -i 's/{{< version >}}/1.18.2/g' $file
+    sed -i 's/{{< relref "doc/\/docs/g' $file
+    sed -i 's/" >}}//g' $file
+    sed -i 's/\*\*Table of Contents\*\*//' $file
+done
+
+for file in `find ./docs/usage/ -name "*.md"`; do
+    # note only works on linux, forget about it when attempting to run on macos
+    # hide hugo toc
+    sed -i 's/title:.*//' $file
+done
+
+for file in docs/*; do
+    if [ -d $file ]; then
+        continue
+    fi
+    if [ "$file" == "docs/intro.md" ]; then
+        continue
+    fi
+    rm $file
+done
+
+
+# for file in `find ./docs/ -name "*.md"`; do
+#     trimmed=$(echo $file | cut -f 2 -d '.')
+#     mv $file .$trimmed.md
+# done
+
+# for file in `find ./i18n/zh-tw/docusaurus-plugin-content-docs/current/ -name "*.md"`; do
+#     trimmed=$(echo $file | cut -f 2 -d '.')
+#     mv $file .$trimmed.md
+# done
+# for file in `find ./i18n/fr-fr/docusaurus-plugin-content-docs/current/ -name "*.md"`; do
+#     trimmed=$(echo $file | cut -f 2 -d '.')
+#     mv $file .$trimmed.md
+# done
+# for file in `find ./i18n/zh-cn/docusaurus-plugin-content-docs/current/ -name "*.md"`; do
+#     trimmed=$(echo $file | cut -f 2 -d '.')
+#     mv $file .$trimmed.md
+# done
