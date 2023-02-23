ee700f7d626e2e038651d3c361528009c019e0f7
techknowlogick <techknowlogick@gitea.io>
2023-02-23 16:45:30 +0800

    Update 'loop_docs-19.sh'

--- loop_docs-19.sh
+++ loop_docs-19.sh
@@ -3,13 +3,13 @@
 set -xe
 
 sed -i 's/<empty/<&#8288;empty/' versioned_docs/version-1.19/advanced/config-cheat-sheet.en-us.md
-#sed -i 's/<empty/<&#8288;empty/' versioned_docs/version-1.19/developers/guidelines-backend.en-us.md
+sed -i 's/<>/<&#8288;>/' versioned_docs/version-1.19/developers/guidelines-backend.en-us.md
 sed -i 's/^url:.*//' versioned_docs/version-1.19/intro.md
 sed -i 's/^title:.*/displayed_sidebar: mySidebar/' versioned_docs/version-1.19/intro.md
 sed -i 's/^slug:.*/slug: \//' versioned_docs/version-1.19/intro.md
 sed -i 's/.\/guidelines-frontend.md/.\/guidelines-frontend/' versioned_docs/version-1.19/developers/hacking-on-gitea.en-us.md
 
-sed -i 's/"version":.*/"version":"1.19.5"/' static/19-swagger.json
+sed -i 's/"version":.*/"version":"1.19.0"/' static/19-swagger.json
 
 for file in `find ./versioned_docs/version-1.19/ -name "*.md"`; do
     # note only works on linux, forget about it when attempting to run on macos
