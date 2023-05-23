f40087381534b64d5d9b3e265661fcfcef6695e2
delvh <leon@kske.dev>
2023-05-24 04:38:02 +0800

    Improve WIP hint (#9)
    Reviewed-on: https://gitea.com/gitea/gitea-docusaurus/pulls/9
    Reviewed-by: techknowlogick <techknowlogick@noreply.gitea.io>
    Reviewed-by: John Olheiser <john+gitea@jolheiser.com>
    Co-authored-by: delvh <leon@kske.dev>
    Co-committed-by: delvh <leon@kske.dev>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,8 +89,8 @@ const config = {
         respectPrefersColorScheme: true,
       },
       announcementBar: {
-        id: 'announcementBar-2', // Increment on change
-        content: `The updated documentation site is a work in progress. If you find any issues, please <a target="_blank" rel="noopener noreferrer" href="https://gitea.com/gitea/gitea-docusaurus/issues">let us know</a>. Translated documentation is also a work in progress.`,
+        id: 'announcementBar-3', // Increment on change
+        content: `This documentation site is fairly new. If you find any issues, please <a target="_blank" rel="noopener noreferrer" href="https://gitea.com/gitea/gitea-docusaurus/issues">let us know</a>. We are currently working on translating the documentation into other languages.`,
       },
       navbar: {
         title: 'Gitea',
