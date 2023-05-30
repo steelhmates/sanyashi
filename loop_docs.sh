d3c11f436312528bff332130c3b93c021c422fd5
jolheiser <john.olheiser@gmail.com>
2023-05-30 17:51:06 +0000

    fix: update sed incantation for empty (#22)
    Should resolve #21
    
    I cannot seem to get this to run properly on my machine, however this is an educated guess because the Chinese translation looks correct.
    
    zh-cn
    https://docs.gitea.com/zh-cn/administration/config-cheat-sheet#repository---release-repositoryrelease
    https://gitea.com/gitea/gitea-docusaurus/src/commit/8c38ae516a72b802cc2889a507c824e0174f5256/loop_docs-zh-cn.sh#L11
    
    en-us
    https://docs.gitea.com/administration/config-cheat-sheet#repository---release-repositoryrelease
    (check diff for current incantation)
    
    Reviewed-on: https://gitea.com/gitea/gitea-docusaurus/pulls/22
    Reviewed-by: techknowlogick <techknowlogick@noreply.gitea.com>
    Co-authored-by: jolheiser <john.olheiser@gmail.com>
    Co-committed-by: jolheiser <john.olheiser@gmail.com>

--- loop_docs.sh
+++ loop_docs.sh
@@ -8,7 +8,7 @@ else
     SED_INPLACE="sed -i ''"
 fi
 
-$SED_INPLACE 's/<empty/<&#8288;empty/' docs/administration/config-cheat-sheet.en-us.md
+$SED_INPLACE 's/\\<empty>/<empty\\>/' docs/administration/config-cheat-sheet.en-us.md
 $SED_INPLACE 's/^url:.*//' docs/intro.md
 $SED_INPLACE 's/^slug:.*/slug: \//' docs/intro.md
 $SED_INPLACE 's/.\/guidelines-frontend.md/.\/guidelines-frontend/' docs/development/hacking-on-gitea.en-us.md
